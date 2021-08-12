54c86b5acc60427a181d9188a5661213a826f297
epuerta <epuerta@utb.edu.co>
2021-08-12 16:21:24 -0500

    Initial Version

---
+++ poc/regular_expression.py
@@ -0,0 +1,12 @@
+import re
+
+
+def patterns(text):
+    text = re.sub(r'#[A-Za-z0-9_]{1,40}', '[HASTAG]', text)
+    text = re.sub(r'@[A-Za-z0-9_]{1,40}', '[MENTION]', text)
+    return text
+
+text = "Why not try something new today? Explore the #PanamaPapers data set with a #graphdatabase sandbox. " \
+       "Easy to get started with, friendly step-by-step guide, all free and no download required! #Neo4j #learning @fulanito"
+
+print(patterns(text))
